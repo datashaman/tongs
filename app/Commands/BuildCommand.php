c558b5963ae8fbdc91ad88241de0ad5380809a61
datashaman <marlinf@datashaman.com>
2020-01-22 10:12:06 +0200

    wip

--- app/Commands/BuildCommand.php
+++ app/Commands/BuildCommand.php
@@ -12,56 +12,33 @@ use LaravelZero\Framework\Commands\Command;
 class BuildCommand extends Command
 {
     /**
-     * The signature of the command.
-     *
      * @var string
      */
     protected $signature = 'build';
 
     /**
-     * The description of the command.
-     *
      * @var string
      */
     protected $description = 'Build static site.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle(Hub $hub)
     {
-        $files = $hub
-            ->pipe($this->prepareFiles())
-            ->then(
-                function ($files) {
-                    return $files
-                        ->map(
-                            function ($file) {
-                                $destination = config('tongs.destination', 'build');
-                                $disk = Storage::disk($destination);
+        $files = $hub->pipe($this->prepareFiles())->then(function ($files) {
+            return $files->map(function ($file) {
+                $destination = config('tongs.destination', 'build');
+                $disk = Storage::disk($destination);
 
-                                $disk->put($file['path'], $file['contents']);
+                $disk->put($file['path'], $file['contents']);
 
-                                return $file;
-                            }
-                        );
-                }
+                return $file;
+            });
+        });
+
+        $this->info(
+            $files->count() .
+                ' files written to ' .
+                Storage::disk(config('tongs.destination', 'build'))->path(''),
         );
-
-        $this->info($files->count() . ' files written to ' . Storage::disk(config('tongs.destination', 'build'))->path(''));
-    }
-
-    /**
-     * Define the command's schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
     }
 
     protected function prepareFiles(): Collection
@@ -69,20 +46,19 @@ class BuildCommand extends Command
         $source = config('tongs.source', 'source');
         $disk = Storage::disk($source);
 
-        return collect($disk->allFiles())
-            ->mapWithKeys(
-                function ($path) use ($disk) {
-                    $file = [
-                        'contents' => $disk->get($path),
-                        'mode' => $this->app['files']->chmod($disk->path($path)),
-                        'stat' => stat($disk->path($path)),
-                        'path' => $path,
-                    ];
+        return collect($disk->allFiles())->mapWithKeys(function ($path) use (
+            $disk
+        ) {
+            $file = [
+                'contents' => $disk->get($path),
+                'mode' => $this->app['files']->chmod($disk->path($path)),
+                'stat' => stat($disk->path($path)),
+                'path' => $path,
+            ];
 
-                    return [
-                        $path => $file,
-                    ];
-                }
-        );
+            return [
+                $path => $file,
+            ];
+        });
     }
 }
